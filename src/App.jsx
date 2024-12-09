374c30ee6984abe782b1d7ec9e883b7d69a41c8d
adamtokola <adamtokola@gmail.com>
2024-12-08 20:48:04 -0500

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
 import './App.css'
 
 function App() {
+  
+  // useState returns an array. And, the array contains two elements. The first is the value that you are storing. The second is a function, which you can use to reset the value. You can deconstruct both of those values from the array
+  const [puppies, setPuppies] = useState(puppyList); //passing the puppyList array from the data.js file as the default value. ALWAYS pass the useState hook a default value.
 
+  console.log(puppies);
+  
   return (
     <>
       <div>
+
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
 
       </div>
     </>
